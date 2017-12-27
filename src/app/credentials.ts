ae225385e573f6541c4ef7114f100b46a010733c
MihowBogucki <mihow.bogucki@gmail.com>
2017-12-27 10:24:26 +0200

    update

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBmFN9zVWzfvISuDT-Hw-1bUOYVynSc_Mc",
+    authDomain: "eventmanager-1521e.firebaseapp.com",
+    databaseURL: "https://eventmanager-1521e.firebaseio.com",
+    projectId: "eventmanager-1521e",
+    storageBucket: "",
+    messagingSenderId: "554477154997"
+  };
