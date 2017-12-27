a84dc2166d54edaf42fd256df1d08ec98d03259c
mihowguitar <mihowguitar@gmail.com>
2017-12-27 20:11:12 +0200

    Update sign up settings

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,25 +1,68 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { EventProvider } from "../../providers/event/event";
+import { Camera } from "@ionic-native/camera";
 
-/**
- * Generated class for the EventDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+@IonicPage({
+  segment: "event-detail/:eventId"
+})
 @Component({
-  selector: 'page-event-detail',
-  templateUrl: 'event-detail.html',
+  selector: "page-event-detail",
+  templateUrl: "event-detail.html"
 })
 export class EventDetailPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  public currentEvent: any = {};
+  public guestName: string = "";
+  public guestPicture: string = null;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public eventProvider: EventProvider,
+    public cameraPlugin: Camera
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EventDetailPage');
+    this.eventProvider
+      .getEventDetail(this.navParams.get("eventId"))
+      .on("value", eventSnapshot => {
+        this.currentEvent = eventSnapshot.val();
+        this.currentEvent.id = eventSnapshot.key;
+      });
   }
 
+  addGuest(guestName: string): void {
+    this.eventProvider
+      .addGuest(
+        guestName,
+        this.currentEvent.id,
+        this.currentEvent.price,
+        this.guestPicture
+      )
+      .then(newGuest => {
+        this.guestName = "";
+        this.guestPicture = null;
+      });
+  }
+
+  takePicture(): void {
+    this.cameraPlugin
+      .getPicture({
+        quality: 95,
+        destinationType: this.cameraPlugin.DestinationType.DATA_URL,
+        sourceType: this.cameraPlugin.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: this.cameraPlugin.EncodingType.PNG,
+        targetWidth: 500,
+        targetHeight: 500,
+        saveToPhotoAlbum: true
+      })
+      .then(
+        imageData => {
+          this.guestPicture = imageData;
+        },
+        error => {
+          console.log("ERROR -> " + JSON.stringify(error));
+        }
+      );
+  }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,26 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import {
-Alert,
-AlertController,
-IonicPage,
-Loading,
-LoadingController,
-NavController
-} from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { EmailValidator } from '../../validators/email';
-import { AuthProvider } from '../../providers/auth/auth';
-import { HomePage } from '../home/home';
+  Alert,
+  AlertController,
+  IonicPage,
+  Loading,
+  LoadingController,
+  NavController
+} from "ionic-angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { EmailValidator } from "../../validators/email";
+import { AuthProvider } from "../../providers/auth/auth";
+import { HomePage } from "../home/home";
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
-
   public loginForm: FormGroup;
-public loading: Loading;
+  public loading: Loading;
 
   constructor(
     public navCtrl: NavController,
@@ -28,57 +27,56 @@ public loading: Loading;
     public alertCtrl: AlertController,
     public authProvider: AuthProvider,
     formBuilder: FormBuilder
-    ) {
-      this.loginForm = formBuilder.group({
-        email: [
-        '',
+  ) {
+    this.loginForm = formBuilder.group({
+      email: [
+        "",
         Validators.compose([Validators.required, EmailValidator.isValid])
-        ],
-        password: [
-        '',
+      ],
+      password: [
+        "",
         Validators.compose([Validators.required, Validators.minLength(6)])
-        ]
-        });
+      ]
+    });
   }
 
-  goToSignup():void {
-    this.navCtrl.push('SignupPage');
-    }
-    goToResetPassword():void {
-    this.navCtrl.push('ResetPasswordPage');
-    }
-    
-    loginUser(): void {
-      if (!this.loginForm.valid) {
+  goToSignup(): void {
+    this.navCtrl.push("SignupPage");
+  }
+  goToResetPassword(): void {
+    this.navCtrl.push("ResetPasswordPage");
+  }
+
+  loginUser(): void {
+    if (!this.loginForm.valid) {
       console.log(
-      `Form is not valid yet, current value: ${this.loginForm.value}`
+        `Form is not valid yet, current value: ${this.loginForm.value}`
       );
-      } else {
+    } else {
       const email = this.loginForm.value.email;
       const password = this.loginForm.value.password;
       this.authProvider.loginUser(email, password).then(
-      authData => {
-      this.loading.dismiss().then(() => {
-      this.navCtrl.setRoot(HomePage);
-      });
-      },
-      error => {
-      this.loading.dismiss().then(() => {
-      const alert: Alert = this.alertCtrl.create({
-      message: error.message,
-      buttons: [{ text: 'Ok', role: 'cancel' }]
-      });
-      alert.present();
-      });
-      }
+        authData => {
+          this.loading.dismiss().then(() => {
+            this.navCtrl.setRoot(HomePage);
+          });
+        },
+        error => {
+          this.loading.dismiss().then(() => {
+            const alert: Alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [{ text: "Ok", role: "cancel" }]
+            });
+            alert.present();
+          });
+        }
       );
       this.loading = this.loadingCtrl.create();
       this.loading.present();
-      }
-      }
-
-    ionViewDidLoad() {
-      console.log('ionViewDidLoad LoginPage');
     }
+  }
 
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad LoginPage");
+  }
 }
