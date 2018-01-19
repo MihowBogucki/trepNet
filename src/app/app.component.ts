32bad7416c93849419a380d2f2ef88e2a383e588
MihowBogucki <mihowguitar@gmail.com>
2018-01-19 23:32:36 +0200

    Menu updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import firebase from 'firebase';
 import { HomePage } from '../pages/home/home';
 import { firebaseConfig } from './credentials';
+import { EventListPage } from '../pages/event-list/event-list';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any;
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = HomePage;
+  activePage: any;
+
+  pages: Array<{title: string, component: any}>;
 
   constructor(
     platform: Platform,
     statusBar: StatusBar,
     splashScreen: SplashScreen
   ) {
+        // used for an example of ngFor and navigation
+        this.pages = [
+          { title: 'Home', component: HomePage },
+          { title: 'Event List', component: EventListPage }
+        ];
+
+        this.activePage = this.pages[0];
+
     firebase.initializeApp(firebaseConfig);
 
     const unsubscribe = firebase.auth().onAuthStateChanged(user => {
@@ -36,4 +50,15 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+    this.activePage = page;
+  }
+
+  checkActive(page){
+    return page = this.activePage;
+  };
 }
